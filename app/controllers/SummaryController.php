d6347b69c93bdeab7da14ef59ad1bd9193e666d8
jigkoxsee <ked.naja@gmail.com>
2014-11-16 12:27:57 +0700

    fix date input on firefox and chrome

--- app/controllers/SummaryController.php
+++ app/controllers/SummaryController.php
@@ -8,10 +8,22 @@ class SummaryController extends BaseController {
   }
 
   public function actionIndex(){
-    $date=Input::get('date');
-    $day=substr($date,-2);
-    $month=substr($date,-5,2);
-    $year=substr($date,0,4);
+    
+    $day=date('d');
+    $month=date('m');
+    $year=date('Y');
+    
+    if(Input::get('isChrome',"0")=="1"){
+      $date=Input::get('date');
+      $day=substr($date,-2);
+      $month=substr($date,-5,2);
+      $year=substr($date,0,4);      
+    }else{
+      $day=Input::get('day');
+      $month=Input::get('month');
+      $year=Input::get('year');
+    }
+
     if(Input::get('summary')==12)//monthly
       return Redirect::to("/summary/$year/$month");
     return Redirect::to("/summary/$year/$month/$day");//daily
--- app/views/summary/index.blade.php
+++ app/views/summary/index.blade.php
@@ -4,12 +4,64 @@
 @stop
 @section('body')
 <div class="well">
+  <?php
+  $isChrome=false;
+  $agent=$_SERVER['HTTP_USER_AGENT'];
+  $isChrome=stristr($agent,"Chrome");
+  ?>
+  
   <h2>ระบุวันที่</h2>
   <form action="/summary" method="POST" role="form">
+
+  @if ($isChrome)
+    <input type="hidden" name="isChrome" value=1>
     <div class="form-group">
       <input type="date" name="date" max="{{date('Y-m-d')}}" value="{{date('Y-m-d')}}" required>
     </div>
-
+    <div class="form-group">
+      <div class="col-xs-12">
+        <div class="col-md-6">
+          <div class="col-md-4">
+            <div class="radio radio-primary">
+              <label>
+                <input name="summary" value="30" checked="" type="radio">
+                <span class="circle"></span><span class="check"></span>
+                รายวัน
+              </label>
+            </div>
+          </div>
+          <div class="col-md-8">
+            <div class="radio radio-primary">
+              <label>
+                <input name="summary" value="12" type="radio">
+                <span class="circle"></span><span class="check"></span>
+                รายเดือน
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>    
+    
+  @else
+    <div class="form-group">
+      <div class="col-xs-12">
+        <div class="col-md-3">
+          <div class="col-md-3">
+            วันที่
+          </div>
+          <div class="col-md-3">
+            <input type="number" required class="form-control" name="day" min=1 max=31 value="{{date('d')}}">
+          </div>
+          <div class="col-md-3">
+            <input type="number" required class="form-control" name="month" min=1 max=12 value="{{date('m')}}">
+          </div>
+          <div class="col-md-3">
+            <input type="number" required class="form-control" name="year" min=1970 max={{date('Y')}} value="{{date('Y')}}">
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="form-group">
       <div class="col-xs-12">
         <div class="col-md-6">
@@ -34,10 +86,8 @@
         </div>
       </div>
     </div>
-    
-    
+  @endif
     <input type="submit" class="btn btn-success" value="ดู">
-    
   </form>
 </div>
 @stop
